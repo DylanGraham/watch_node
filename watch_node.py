5682c85f18e92f0ae86ddf2ca1fb8232c896fb81
Dylan Graham <dylan.graham@gmail.com>
2016-08-11 10:16:38 +1000

    Add qstat -n flag

--- watch_node.py
+++ watch_node.py
@@ -42,7 +42,7 @@ def check_nodes():
         with open(watch_node_file, 'r') as fp:
             nodelist = fp.read().split()
             for node in nodelist:
-                p1 = subprocess.Popen(["qstat | grep " + node], shell=True, stdout=subprocess.PIPE)
+                p1 = subprocess.Popen(["qstat -n | grep " + node], shell=True, stdout=subprocess.PIPE)
                 if p1.communicate()[0]:
                     send_email(node)
                     nodelist.remove(node)
